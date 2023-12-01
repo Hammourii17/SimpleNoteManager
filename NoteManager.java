bd4e832dd3ee7cb5b6644654bd66e7812f4738db
Hammourii17 <a-7moory@hotmail.com>
2023-12-01 19:14:54 +0300

    second is here again

--- NoteManager.java
+++ NoteManager.java
@@ -23,7 +23,7 @@ public class NoteManager {
     // Main method for demonstration purposes
     public static void main(String[] args) {
         NoteManager manager = new NoteManager();
-        manager.addNote("hot fix was done ");
+        manager.addNote("hot fix was done and second collaborator was here again");
         manager.addNote("Second note");
         manager.displayNotes();
     }
